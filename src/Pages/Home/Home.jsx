bf93636c3dd238de687550ae09c3aad74795d8d2
123Somrat <jafaruddin875@gmail.com>
2023-11-05 16:14:27 +0100

    add icon and add books category

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Banner from '../../Component/Banner/Banner'
+import BookesCategory from '../../Component/BookesCategory/BookesCategory'
 
 export default function Home() {
   return (
     <div>
+      
+       <section>
        <Banner/>
+       </section>
+      <section>
+         <BookesCategory/>
+      </section>
       </div>
   )
 }
