361b666745efa4147dc0c0024f72c67326f6d399
123Somrat <jafaruddin875@gmail.com>
2023-11-06 00:25:43 +0100

    show all books

--- src/Component/Books/Books.jsx
+++ src/Component/Books/Books.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Book from './Book'
 
 export default function Books({books}) {
     
   return (
-    <div>
+    <div className='grid justify-center md:mx-8 md:grid-cols-2 lg:grid-cols-3 gap-4 my-8'>
       {
-         
+         books?.map(book=><Book
+         key={book._id}
+         book={book}
+         />)
       }
 
     </div>
