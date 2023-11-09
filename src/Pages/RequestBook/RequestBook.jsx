dc50f4d43578003112ab500b8379377e3669f945
123Somrat <jafaruddin875@gmail.com>
2023-11-09 17:45:43 +0100

    Used helmet also space and requestBook page

--- src/Pages/RequestBook/RequestBook.jsx
+++ src/Pages/RequestBook/RequestBook.jsx
@@ -1,8 +1,14 @@
-import Lottie from "lottie-react";
+import Lottie from "lottie-react"
+import { Helmet } from "react-helmet";
 import ComeingSoon from "../../../public/Animation - 1699535547055.json";
 export default function RequestBook() {
   return (
     <div>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Request Book</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
       <Lottie animationData={ComeingSoon} loop={true} className="h-[555px]" />
     </div>
   );
--- src/Pages/Spaces/Spaces.jsx
+++ src/Pages/Spaces/Spaces.jsx
@@ -1,8 +1,14 @@
 import Lottie from "lottie-react"
+import { Helmet } from "react-helmet"
 import ComeingSoon from "../../../public/Animation - 1699534129054.json"
 export default function Spaces() {
   return (
     <div className="max-w-6xl my-10">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Space & Rooms</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
         <Lottie animationData={ComeingSoon} loop={true} className="h-[475px]"/>
     </div>
   )
