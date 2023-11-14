f6ef7cd3d8514e3abc2d69d0fb2523bd97ae49ae
123Somrat <jafaruddin875@gmail.com>
2023-11-14 13:27:17 +0100

    design the header section of spaces and rooms page

---
+++ src/Pages/Spaces/SpaceAndRoomCover/SpaceAndRoomCover.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./SpaceAndRoomCover.css"
+export default function SpaceAndRoomCover() {
+  return (
+    <>
+        <div className='bg_img'>
+        <h1 className='text-5xl text-white brightness-200 text-center pt-28 mb-2'>Spaces and Room</h1>
+        <h1 className='text-center text-white'>Home / Spaces & Rooms</h1>
+        </div>
+        <h1 className='text-5xl mt-10 md:mx-10 mb-4'>Explore Our Facilities</h1>
+        <hr className='border-1 md:mx-10  p-[1px] outline-none bg-gradient-to-r from-amber-400 to-amber-700'></hr>
+    </>
+  )
+}
--- src/Pages/Spaces/Spaces.jsx
+++ src/Pages/Spaces/Spaces.jsx
@@ -1,6 +1,7 @@
 import Lottie from "lottie-react"
 import { Helmet } from "react-helmet"
 import ComeingSoon from "../../../public/Animation - 1699534129054.json"
+import SpaceAndRoomCover from "./SpaceAndRoomCover/SpaceAndRoomCover"
 export default function Spaces() {
   return (
     <div className="max-w-6xl my-10">
@@ -9,7 +10,9 @@ export default function Spaces() {
           <title>Space & Rooms</title>
           <link rel="canonical" href="http://mysite.com/example" />
         </Helmet>
-        <Lottie animationData={ComeingSoon} loop={true} className="h-[475px]"/>
+        <section>
+          <SpaceAndRoomCover/>
+        </section>
     </div>
   )
 }
