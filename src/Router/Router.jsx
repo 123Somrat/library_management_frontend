6ba6ccb32cb5ca5e1ffa9d8856b82780c50e0dbd
123Somrat <jafaruddin875@gmail.com>
2023-12-20 21:16:52 +0100

    change port number

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -37,13 +37,13 @@ const router = createBrowserRouter([
           </PrivateRoute>
         ),
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/bookdetails/${params.id}`),
+          fetch(`http://localhost:3000/bookdetails/${params.id}`),
       },
       {
         path: "/readbook/:id",
         element: <ReadingBook />,
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/readbook/${params.id}`),
+          fetch(`http://localhost:3000/readbook/${params.id}`),
       },
       {
         path: "/borrowedBook",
